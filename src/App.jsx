ae103542e93f1e006062f05b200ad1fe322d578a
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-01 21:08:41 +0200

    [wip: src/app.jsx] env setup

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
+    <div className="min-h-screen">
+      <div className='gradient-bg-welcome'>
+        <Navbar />
+        <Welcome/>  
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
     </div>
   )
 }
