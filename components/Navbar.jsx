b9fe7939ca7347dfd7448fdc3b57feeba5780a4c
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-03 19:09:22 +0200

    [feat: Navbar.jsx] adding menu on navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 import logo from "/images/logo.png";
@@ -13,18 +13,37 @@ const NavbarItem = ({title,classProps}) =>{
 }
 
 const Navbar = () => {
+
+
+  const [toggleMenu,setToggleMenu] = React.useState(false);
+
+
   return (
-    <nav className="w-full flex md:justify-center justify-between items-center p-4 ">
+    <nav className="w-full flex md:justify-center  items-center p-4 ">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="Logo" className="w-32 cursor-pointer" />
       </div>
-      <ul className="text-white md:flex hidden list-none flex-row justify-center items-center flex-initial ">
+      <ul className="text-white md:flex  list-none flex-row justify-center items-center flex-initial ">
           {
           ["Market", "Exchange", "Tutorials", "Wallets"].map((item,index)=>(
               <NavbarItem key={item + index} title={item}  />
           ))
           }
+          <li className="bg-blue-600 py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-blue-500">
+            Login
+          </li>
       </ul>
+      <div className="flex relative">
+          {
+          toggleMenu ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={()=> setToggleMenu(false)}/> : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={()=> setToggleMenu(true)} />}
+          {
+            toggleMenu && (
+              <ul className="text-xl w-full my-2 ">
+                <AiOutlineClose onClick={()=> setToggleMenu(false)} />
+              </ul>
+            )
+          }
+      </div>
     </nav>
   );
 };
