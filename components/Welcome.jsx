40c76923935a8b464b94b9cde3660dc2fff55df6
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-07 19:59:38 +0200

    [feat: Welcome.jsx] adding message box

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -7,6 +7,18 @@ import { Loader } from "./";
 const commonStyles =
   "sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-2 border-gray-400  text-white";
 
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    name={name}
+    value={value}
+    handleChange={(e) => handleChange(e, name)}
+    step="0.0001"
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm bg-gray-700"
+  />
+);
+
 const Welcome = () => {
   const connectWallet = () => {};
   return (
@@ -72,18 +84,21 @@ const Welcome = () => {
                 <BsInfoCircle fontSize={17} color="#fff" />
               </div>
               <div>
-                 <p className="text-white font-light text-sm">
-                     Address
-                 </p>
-                 <p className="text-white font-semibold text-lg mt-1">
-                     Etherium
-                 </p>
+                <p className="text-white font-light text-sm">Address</p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Etherium
+                </p>
               </div>
             </div>
           </div>
-              <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center bg-gray-700 rounded-full">
-
-              </div>
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center bg-gray-700 rounded-lg">
+            <Input
+              placeholder="Address To"
+              name="addressTo"
+              type="text"
+              handleChange={() => {}}
+            />
+          </div>
         </div>
       </div>
     </div>
